a5c160738bc4d7eb4ee6eccf6f614bec71887334
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-10 02:21:44 +0530

    FloatingActionButton (FAB)

--- app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
+++ app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
@@ -1,20 +1,52 @@
 package eu.tutorials.mywishlistapp
 
-
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 
 @Composable
 fun HomeView(
 ){
 
+    val context = LocalContext.current
     Scaffold(
-        topBar = {AppBarView(title= "WishList")},
+        topBar = {AppBarView(title= "WishList", {
+            Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+
+        })},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {
+                    Toast.makeText(context, "FAButton Clicked", Toast.LENGTH_LONG).show()
+                    // TODO Add Navigation to add screen
+                }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
     ) {
         LazyColumn(modifier = Modifier
             .fillMaxSize()
