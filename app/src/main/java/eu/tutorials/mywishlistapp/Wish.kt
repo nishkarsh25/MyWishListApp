0353d42b45747ccec14597ec32fa574b89e80240
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-10 02:24:21 +0530

    Displaying Dummy Data

--- app/src/main/java/eu/tutorials/mywishlistapp/Wish.kt
+++ app/src/main/java/eu/tutorials/mywishlistapp/Wish.kt
@@ -5,3 +5,16 @@ data class Wish(
     val title: String="",
     val description:String=""
 )
+
+object DummyWish{
+    val wishList = listOf(
+        Wish(title="Google Watch 2",
+            description =  "An android Watch"),
+        Wish(title = "Oculus Quest 2",
+            description = "A VR headset for playing games"),
+        Wish(title = "A Sci-fi, Book",
+            description= "A science friction book from any best seller"),
+        Wish(title = "Bean bag",
+            description = "A comfy bean bag to substitute for a chair")
+    )
+}
