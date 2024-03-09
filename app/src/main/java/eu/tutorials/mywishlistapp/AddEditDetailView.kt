4b9988d2bdfc1f3ed7361558fa5c5d2fe1df6f87
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-10 02:34:21 +0530

    Starting with the AddEditDetailView

---
+++ app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
@@ -0,0 +1,61 @@
+package eu.tutorials.mywishlistapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun AddEditDetailView(
+    id: Long,
+    viewModel: WishViewModel,
+    navController: NavController
+){
+    Scaffold(topBar = {AppBarView(title =
+    if(id != 0L) stringResource(id = R.string.update_wish)
+    else stringResource(id = R.string.add_wish)
+    ) {navController.navigateUp()}
+    },
+
+        ) {
+        Column(modifier = Modifier
+            .padding(it)
+            .wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+        }
+    }
+
+}
+
+
+
+
+
